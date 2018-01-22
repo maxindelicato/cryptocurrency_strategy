199e4724d1bf47ee0fb7732fb022af6c365522e0
Max Indelicato <max@indelicato.cc>
2018-01-21 22:56:20 -0500

    Initial commit

---
+++ src/http.h
@@ -0,0 +1,104 @@
+#pragma once
+
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <boost/asio/connect.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/asio/ssl/error.hpp>
+#include <boost/asio/ssl/stream.hpp>
+#include <boost/beast/core.hpp>
+#include <boost/beast/http.hpp>
+#include <boost/beast/version.hpp>
+#include "../lib/json/json.h"
+#include "root_certificates.hpp"
+
+
+using tcp = boost::asio::ip::tcp;       // from <boost/asio/ip/tcp.hpp>
+namespace ssl = boost::asio::ssl;       // from <boost/asio/ssl.hpp>
+namespace http = boost::beast::http;    // from <boost/beast/http.hpp>
+
+namespace cryptocurrency_strategy {
+
+using json = nlohmann::json;
+
+std::tuple<int, json> get_poloniex_ticker(const std::string currency_pair) {
+    try
+    {
+        auto const host = "poloniex.com";
+        auto const port = "443";
+        auto const target = "/public?command=returnTicker";
+        int version = 11;
+
+        // The io_context is required for all I/O
+        boost::asio::io_context ioc;
+        // The SSL context is required, and holds certificates
+        ssl::context ctx{ssl::context::sslv23_client};
+
+        // This holds the root certificate used for verification
+        load_root_certificates(ctx);
+
+        // These objects perform our I/O
+        tcp::resolver resolver{ioc};
+        ssl::stream<tcp::socket> stream{ioc, ctx};
+
+        // Set SNI Hostname (many hosts need this to handshake successfully)
+        if(! SSL_set_tlsext_host_name(stream.native_handle(), host))
+        {
+            boost::system::error_code ec{static_cast<int>(::ERR_get_error()), boost::asio::error::get_ssl_category()};
+            throw boost::system::system_error{ec};
+        }
+
+        // Look up the domain name
+        auto const results = resolver.resolve(host, port);
+
+        // Make the connection on the IP address we get from a lookup
+        boost::asio::connect(stream.next_layer(), results.begin(), results.end());
+
+        // Perform the SSL handshake
+        stream.handshake(ssl::stream_base::client);
+
+        // Set up an HTTP GET request message
+        http::request<http::string_body> req{http::verb::get, target, version};
+        req.set(http::field::host, host);
+
+        // Send the HTTP request to the remote host
+        http::write(stream, req);
+
+        // This buffer is used for reading and must be persisted
+        boost::beast::flat_buffer buffer;
+
+        // Declare a container to hold the response
+        http::response<http::dynamic_body> res;
+
+        // Receive the HTTP response
+        http::read(stream, buffer, res);
+
+        // Write the message to standard out
+        std::cout << res << std::endl;
+
+        // Gracefully close the stream
+        boost::system::error_code ec;
+        stream.shutdown(ec);
+        if(ec == boost::asio::error::eof)
+        {
+            // Rationale:
+            // http://stackoverflow.com/questions/25587403/boost-asio-ssl-async-shutdown-always-finishes-with-an-error
+            ec.assign(0, ec.category());
+        }
+        // Ignore "short read". It occurs when the server-side tears down the SSL connection early
+        if(ec && ec.message() != "short read")
+            throw boost::system::system_error{ec};
+
+        // If we get here then the connection is closed gracefully
+//        std::cout << "result: " << std::string(res.body().data().begin(), res.body().data().end(), ) << std::endl;
+        return std::make_tuple(1, json::parse(boost::beast::buffers_to_string(res.body().data()))["BTC_ETH"]);
+    }
+    catch(std::exception const& e)
+    {
+        std::cerr << "Error: " << e.what() << std::endl;
+        return std::make_tuple(0, json());
+    }
+}
+
+}
---
+++ src/main.cpp
@@ -0,0 +1,71 @@
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <boost/asio.hpp>
+#include <boost/bind.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include <boost/lexical_cast.hpp>
+#include "xtensor/xarray.hpp"
+#include "xtensor/xeval.hpp"
+#include "xtensor/xtensor.hpp"
+#include "xtensor/xio.hpp"
+#include "xtensor/xindex_view.hpp"
+#include "xtensor/xmath.hpp"
+#include "../lib/json/json.h"
+#include "../lib/signaldata/technical_indicator/simple_moving_average.h"
+#include "http.h"
+
+namespace ti = signaldata::technical_indicator;
+namespace cs = cryptocurrency_strategy;
+using boost::lexical_cast;
+
+const int ticked_count = 60;
+const int deadline_timer_interval = 60; // seconds
+
+const xt::xtensor<double, 1> add(const xt::xtensor<double, 1> data, const double val) {
+    auto data_size = static_cast<int>(data.shape()[0]);
+    // A new xtensor always starts with a single value, so handle that here
+    if (data_size == 1 && data(1, 0) == 0.0)
+        return xt::xtensor<double, 1>({val});
+
+    xt::xtensor<double, 1> new_data = xt::zeros<double>({1, data_size + 1});
+    std::copy(data.begin(), data.end(), new_data.begin());
+    new_data(1, data_size) = val;
+    return new_data;
+}
+
+void tick(const boost::system::error_code& sec, boost::asio::deadline_timer& timer, int& count, xt::xtensor<double, 1>& last_data) {
+    if (count >= ticked_count)
+        return;
+
+    ++count;
+
+    auto[ec, js] = cs::get_poloniex_ticker("BTC_ETH");
+
+    std::cout << "last: " << lexical_cast<double>(js["last"].get<std::string>()) << std::endl;
+
+    last_data = add(last_data, lexical_cast<double>(js["last"].get<std::string>()));
+    std::cout << "last_data: " << last_data << std::endl;
+
+    // This does a simple moving average, but could use any of the time series algos
+    // in ../lib/signaldata/technical_indicator (I wrote that lib!)
+    auto last_sma_data = ti::simple_moving_average(last_data, 3);
+    std::cout << "last_sma_data: " << last_sma_data << std::endl;
+
+    timer.expires_at(timer.expires_at() + boost::posix_time::seconds(deadline_timer_interval));
+    timer.async_wait(boost::bind(tick, boost::asio::placeholders::error, boost::ref(timer), boost::ref(count), boost::ref(last_data)));
+}
+
+int main() {
+    int count = 0;
+    xt::xtensor<double, 1> last_data;
+    boost::asio::io_service ios;
+    boost::asio::deadline_timer timer(ios, boost::posix_time::seconds(deadline_timer_interval));
+
+    timer.async_wait(boost::bind(tick, boost::asio::placeholders::error, boost::ref(timer), boost::ref(count), boost::ref(last_data)));
+    ios.run();
+
+    std::cout << "Done." << std::endl;
+
+    return 0;
+}
